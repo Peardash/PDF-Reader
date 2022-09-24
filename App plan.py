a3d8c70b2a0e67e9ff1262f611375f5cbec7587f
Peardash <Rest4ations@gmail.com>
2022-09-24 00:21:48 -0400

    Sep 24 First edits

---
+++ App plan.py
@@ -0,0 +1,7 @@
+# open txt or pdf file
+# create display
+# put txt or pdf file on display
+# highlight one word
+# write a loop to shift from word to word
+# highlight each word
+# close application
