f4f7cf94116c819520d4df88fdf915b92b16a3c1
Peardash <Rest4ations@gmail.com>
2022-09-24 00:22:45 -0400

    Sep 24 First Edits

--- Rough.py
+++ Rough.py
@@ -1,12 +1,13 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import os
+from tkinter import *
 
 # making text file from pdf and printing all contents on it in the same folder under the name Lorem.txt
 # we need pdffilereader and pdffilewriter functions from PyPDF2 library
-file_path = 'Lorem.pdf'
+file_path = 'Bro.pdf'
 pdf = PdfFileReader(file_path)
-
-with open('Lorem Note.txt', 'w') as f:
+yo = ""
+with open('Bro.txt', 'w', encoding="utf-8") as f:
     for page_num in range(pdf.numPages):
         print('Page: {0}'.format(page_num))
         pageObj = pdf.getPage(page_num)
@@ -21,19 +22,35 @@ with open('Lorem Note.txt', 'w') as f:
             f.write('Page {0}\n'.format(page_num+1))
             f.write(''.center(100, '-'))
             f.write(txt)
+            yo += txt
     f.close()
 
-file_path = input("Enter the file path: ")
-while not os.path.exists(file_path):
-    file_path = input("The path does not exist, try entering the file path again: ")
-
+# file_path = input("Enter the file path: ")
+# while not os.path.exists(file_path):
+#     file_path = input("The path does not exist, try entering the file path again: ")
+file_path = 'C:\\Users\\Erazmo\\Desktop\\PDFReader\\Bro.txt'
 with open(file_path, mode='rt', encoding='utf-8') as f:
     text = f.read()
 
-search_word = input("Enter the word you want to search: ")
+# word searching
 
-if search_word in text:
-    print()
-    print(text.replace(search_word, '\033[44;33m{}\033[m'.format(search_word)))
-else:
-    print("Word not found")
+# search_word = input("Enter the word you want to search: ")
+
+# if search_word in text:
+#     print()
+#     print(text.replace(search_word, '\033[44;33m{}\033[m'.format(search_word)))
+# else:
+#     print("Word not found")
+
+root = Tk()
+
+# creating label widget
+
+myLabel = Label(root, text= yo)
+
+# shoving it to the screen
+
+myLabel.pack()
+
+# loop for the graphical user interface
+root.mainloop()
